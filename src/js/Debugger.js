b323d1a87e7d759bd792b42f1bf3f9b0e8363340
Victor Gabriel <victorgabrielss20@gmail.com>
2022-07-17 10:48:42 -0300

    emulador funcionando pipeline, não testado direito

--- src/js/Debugger.js
+++ src/js/Debugger.js
@@ -1,27 +1,3 @@
-function Step() {
-	MovePCDOM();
-	PipelineClock();
-}
-
-function HandleFileDrop(ev) {
-	ev.preventDefault();
-	console.log(ev);
-	if (ev.dataTransfer.items) {
-		// Use DataTransferItemList interface to access the file(s)
-		for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-		  // If dropped items aren't files, reject them
-		  if (ev.dataTransfer.items[i].kind === 'file') {
-			const file = ev.dataTransfer.items[i].getAsFile();
-			console.log('... file[' + i + '].name = ' + file.name);
-		}
-		}
-	}
-}
-
-function HandleFileDragOver(ev) {
-	ev.preventDefault();
-	// console.log(ev);
-}
 const inp = document.getElementById("file_input");
 inp.addEventListener('change', async ev => {
 	let ab = await inp.files[0].arrayBuffer()
--- src/js/Emulator.js
+++ src/js/Emulator.js
@@ -6,19 +6,21 @@ const CPU = {
 	// Pipeline Stages Registers
 	pipeline: {
 		if_id: { pc:0, ir:0 },
-		id_ex: { pc:0, ir:0, reg_out_a: 0, reg_out_b: 0, imm:0, flags: {
-			alu_op: 0,
-			write_mem: 0,
-			read_mem: 0,
-			write_back: 0
-		}},
-		ex_mem: { pc:0, ir:0, reg_out_a: 0, reg_out_b: 0, imm:0, flags: {
-			alu_op: 0,
-			write_mem: 0,
-			read_mem: 0,
-			write_back: 0
-		}},
-		mem_wb: {}
+		id_ex:{
+			pc: 0, ir: 0, rs: 0, rt: 0, rd: 0, shamt: 0, imm: 0, addr: 0, is_i: false,
+			alu_op: 0, write_back: false, write_mem:false, read_mem: false, wb_dest: 0
+		},
+		ex_mem: {
+			pc: 0, ir: 0,
+			wb_dest: 0, 
+			rt: 0, alu_out: 0, write_mem: false, read_mem: false, write_back: false
+		},
+		mem_wb: {
+			pc: 0, ir: 0,
+			wb_dest: 0,
+			wb_data: 0,
+			write_back: false
+		}
 	}
 }
 
@@ -82,17 +84,19 @@ function ExecuteInstruction() {
 function InstructionFetch() {
 	CPU.pipeline.if_id.ir = ReadWord(CPU.pc);
 	CPU.pipeline.if_id.pc = CPU.pc;
-	CPU.pc += 4;
+	// CPU.pc += 4;
 }
 
 function InstructionDecode() {
 	instruction = CPU.pipeline.if_id.ir;
+	let pc = CPU.pipeline.if_id.pc;
 
 	let write_back = false;
 	let wb_dest = 0;
 	let read_mem = false;
 	let alu_op = 0;
 	let write_mem = false;
+	let is_i = false;
 
 	// Instruction Decode / Register Reading
 	let op = 	 instruction >> 26 & 0b111111;
@@ -115,24 +119,28 @@ function InstructionDecode() {
 			break;
 		case 0x08: // ADDI
 			// CPU.reg[rt] = CPU.reg[rs] + im;
-			write_back = 1;
-			wb_dest = rt;
-			break;
-		case 0x04: // BEQ
-			CPU.pc += (CPU.reg[rt] === CPU.reg[rs])?im<<2:0;
-			break;
-		case 0x05: // BNQ
-			CPU.pc += (CPU.reg[rt] !== CPU.reg[rs])?im<<2:0;
-			break;
-		case 0x23: // LW
-			write_back = 1;
-			read_mem = 1;
+			write_back = true;
 			alu_op = 0x20;
 			wb_dest = rt;
+			is_i = true;
+			break;
+		case 0x04: // BEQ
+			CPU.pc += (CPU.reg[rt] === CPU.reg[rs])?imm<<2:0;
+			break;
+		case 0x05: // BNQ
+			CPU.pc += (CPU.reg[rt] !== CPU.reg[rs])?imm<<2:0;
+			break;
+		case 0x23: // LW
+			write_back = true;
+			read_mem = true;
+			alu_op = 0x20;
+			wb_dest = rt;
+			is_i = true;
 			break;
 		case 0x2B: // SW
 			alu_op = 0x20;
-			write_mem = 1;
+			write_mem = true;
+			is_i = true;
 			break;
 		case 0x02: // J
 			CPU.pc += addr << 2;
@@ -146,68 +154,93 @@ function InstructionDecode() {
 	}
 
 	CPU.pipeline.id_ex = {
-		...CPU.pipeline.if_id,
-		rs, rt, rd, shamt, imm, addr,
-		flags: { alu_op, write_back, write_mem, read_mem }
+		pc, ir: instruction,
+		rs, rt, rd, shamt, imm, addr, is_i,
+		alu_op, write_back, write_mem, read_mem, wb_dest
 	};
 }
 
 function InstructionExecution() {
 	let alu_out = 0;
-	let in_a = CPU.pipeline.id_ex.reg_out_a;
-	let in_b = CPU.pipeline.id_ex.reg_out_b;
+	let { pc, ir, rs, rt, rd, shamt, imm, addr, alu_op, is_i,
+		write_back, write_mem, read_mem, wb_dest } = CPU.pipeline.id_ex;
 	
-	if (!CPU.pipeline.id_ex.is_r)
-		in_b = CPU.pipeline.id_ex.imm;
+	let in_a = CPU.reg[rs];
+	let in_b = CPU.reg[rt];
 
+	if (is_i) {
+		in_b = imm;
+	}
 	
-	switch (CPU) {
+	switch (alu_op) {
 		case 0x20:
-			alu_out = rs + rt;
+			alu_out = in_a + in_b;
 			break;
 		case 0x22:
-			alu_out = rs - rt;
+			alu_out = in_a - in_b;
 			break;
 		case 0x24:
-			alu_out = rs & rt;
+			alu_out = in_a & in_b;
 			break;
 		case 0x25:
-			alu_out = rs | rt;
+			alu_out = in_a | in_b;
 			break;
 		case 0x00:
-			alu_out = rs << shamt;
+			alu_out = CPU.reg[rt] << shamt;
 			break;
 		case 0x02:
-			alu_out = rt >> shamt;
+			alu_out = CPU.reg[rt] >> shamt;
 			break;
 		default:
 			break;
 	}
 
 	CPU.pipeline.ex_mem = {
-		flags: CPU.pipeline.id_ex.flags,
-		rt, alu_out
+		pc, ir, wb_dest, rt, alu_out, write_mem, read_mem, write_back
 	}
 }
 
 function AccessMemory() {
-	if (CPU.pipeline.ex_mem.write_mem) {
+	let { pc, ir, wb_dest, write_back, rt, alu_out, write_mem, read_mem } = CPU.pipeline.ex_mem;
+	let wb_data = alu_out;
+	if (write_mem) {
 		StoreWord(alu_out,rt);
 	}
-	if (CPU.pipeline.ex_mem.flags.read_mem) {
+	if (read_mem) {
+		let w = ReadWord(alu_out);
+		wb_data = w;
+	}
 
+	CPU.pipeline.mem_wb = {
+		pc, ir,
+		wb_dest,
+		write_back,
+		wb_data
 	}
 }
 
+function WriteBack() {
+	let { write_back, wb_dest, wb_data } = CPU.pipeline.mem_wb;
+	if (write_back)
+		CPU.reg[wb_dest] = wb_data;
+}
+
 function PipelineClock() {
+	WriteBack();
+	MoveStageDOM(4, CPU.pipeline.mem_wb.pc);
+	
 	AccessMemory();
+	MoveStageDOM(3, CPU.pipeline.ex_mem.pc);
 	// UpdateMemory(?)
 	InstructionExecution();
+	MoveStageDOM(2, CPU.pipeline.id_ex.pc);
 	//
 	InstructionDecode();
+	MoveStageDOM(1, CPU.pipeline.id_ex.pc);
 	//
 	InstructionFetch();
-	//
+	MoveStageDOM(0, CPU.pipeline.if_id.pc);
+	CPU.pc += 4;
 }
 
 function ReadWord(address) {
